ca43e7c9fbd855d2c91f5c34557aa91950d26023
Hamza-benjelloun <hamza.benjelloun@octo.ma>
2022-11-04 08:27:27 +0100

    Cleaning app.py

--- app.py
+++ app.py
@@ -14,14 +14,6 @@ from flask_cors import CORS, cross_origin
 app = Flask(__name__,static_folder='./build',static_url_path='')
 CORS(app)
 
-
-# @app.after_request
-# def after_request(response):
-#     response.headers.add("Access-Control-Allow-Origin", "*")
-#     response.headers.add("Access-Control-Allow-Headers", "Content-Type,Authorization")
-#     response.headers.add("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS")
-#     return response
-
 @app.route("/api", methods=["POST"])
 async def beinmatch():
     agent = UserAgent(verify_ssl=False)
